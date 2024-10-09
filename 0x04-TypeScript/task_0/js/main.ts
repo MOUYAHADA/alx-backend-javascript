60c972ac5aee07a850cd6371cb1f5d2e70555bd4
Mohamed Mouyahada <mouyahada@gmail.com>
2024-10-09 20:05:26 +0100

    task 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -23,21 +23,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const studentsList: Student[] = [studentOne, studentTwo];
 
   const table = document.createElement("table");
+  table.style.borderCollapse = "collapse";
+  table.style.width = "100%"; 
+
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+
+  const thFirstName = document.createElement('th');
+  thFirstName.innerText = 'First Name';
+  const thLocation = document.createElement('th');
+  thLocation.innerText = 'Location';
+
+  headerRow.append(thFirstName, thLocation);
+  thead.append(headerRow);
+  table.append(thead);
 
   const tbody = document.createElement("tbody");
 
-  studentsList.forEach((student) => {
-    const tr = document.createElement('tr')
-    const firstName = document.createElement('td')
-    const location = document.createElement('td')
-    firstName.innerText = student.firstName
-    location.innerText = student.location
+  studentsList.forEach(({ firstName, location }) => {
+    const row = document.createElement('tr');
+    
+    const tdFirstName = document.createElement('td');
+    tdFirstName.innerText = firstName;
 
-    tr.append(firstName, location)
-    tbody.append(tr);
+    const tdLocation = document.createElement('td');
+    tdLocation.innerText = location;
+
+    row.append(tdFirstName, tdLocation);
+    tbody.append(row);
   });
 
-  table.append(tbody)
-
-  document.body.appendChild(table)
-})
+  table.append(tbody);
+  document.body.appendChild(table);
+});
