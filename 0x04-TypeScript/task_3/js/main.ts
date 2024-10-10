febc37ab0899cd893146785d7d201d0431818566
Mohamed Mouyahada <mouyahada@gmail.com>
2024-10-10 01:31:38 +0100

    task 3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,16 @@
+/// <reference path="./crud.d.ts">
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud.js";
+
+const row: RowElement = {
+	firstName: "Guillaume",
+	lastName: "Salva",
+};
+
+const newRowId: RowID = CRUD.insertRow(row);
+
+const updateRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowId, updateRow);
+
+CRUD.deleteRow(newRowId)
